49b54ca1843017c455676825bbe825859594fbd0
David Schissler <david.schissler@gmail.com>
2016-01-11 14:07:51 +0200

    'Change formatting of webpack config to use two space indentation.'

--- dev/gulpfile.webpack.js
+++ dev/gulpfile.webpack.js
@@ -35,10 +35,10 @@ let appConfig = yaml.load(fs.readFileSync(webpackRoot + "/config.yml", 'utf8'));
  */
 let entryMap = {};
 for (let common of getNamesFromDirectory(`${webpackRoot}/commons`)) {
-    entryMap[`commons/${common}`] = `./commons/${common}`;
+  entryMap[`commons/${common}`] = `./commons/${common}`;
 }
 for (let entry of getNamesFromDirectory(`${webpackRoot}/entries`)) {
-    entryMap[`entries/${entry}`] = `./entries/${entry}`;
+  entryMap[`entries/${entry}`] = `./entries/${entry}`;
 }
 
 /**
@@ -46,232 +46,232 @@ for (let entry of getNamesFromDirectory(`${webpackRoot}/entries`)) {
  */
 let commonsChunkPluginArr = [];
 for (let commonName in appConfig.commons) {
-    let entryArrPath = appConfig.commons[commonName].map(function(entryName) {
-        return `entries/${entryName}`;
-    });
-    commonsChunkPluginArr.push(new CommonsChunkPlugin({
-        name    : `commons/${commonName}`,
-        filename: 'js/[name].js',
-        chunks  : entryArrPath
-    }));
+  let entryArrPath = appConfig.commons[commonName].map(function(entryName) {
+    return `entries/${entryName}`;
+  });
+  commonsChunkPluginArr.push(new CommonsChunkPlugin({
+    name    : `commons/${commonName}`,
+    filename: 'js/[name].js',
+    chunks  : entryArrPath
+  }));
 }
 
 /**
  *
  */
 let wpConf = {
-    cache: true,
-    context: webpackRoot,
-    entry: entryMap,
-    output: {
-        path              : "/tmp/webird-" + projectRootHash + "-webpack",
-        publicPath        : '/',
-        filename          : 'js/[name].js',
-        chunkFilename     : 'js/chunk/[id].js',
-        namedChunkFilename: 'js/[name].js'
+  cache: true,
+  context: webpackRoot,
+  entry: entryMap,
+  output: {
+    path              : "/tmp/webird-" + projectRootHash + "-webpack",
+    publicPath        : '/',
+    filename          : 'js/[name].js',
+    chunkFilename     : 'js/chunk/[id].js',
+    namedChunkFilename: 'js/[name].js'
+  },
+  resolve: {
+    root: [appModulesRoot, bowerRoot, nodeModulesRoot, themeRoot],
+    modulesDirectories: [appModulesRoot, 'node_modules', 'bower_components'],
+    alias: {
+      underscore: 'lodash',
+      handlebars: 'handlebars/dist/handlebars',
+      highlight: 'highlight.js/lib/highlight'
     },
-    resolve: {
-        root: [appModulesRoot, bowerRoot, nodeModulesRoot, themeRoot],
-        modulesDirectories: [appModulesRoot, 'node_modules', 'bower_components'],
-        alias: {
-            underscore: 'lodash',
-            handlebars: 'handlebars/dist/handlebars',
-            highlight: 'highlight.js/lib/highlight'
-        },
-        extensions: [
-            '',
-            '.js', '.coffee',
-            '.html', '.nunj',
-            '.css', '.scss', '.less',
-            '.json', '.yml'
-        ]
-    },
-    resolveLoader: {
-        root: nodeModulesRoot
-    },
-    plugins: [
-        new DefinePlugin({
-            VERSION: JSON.stringify(require(devRoot + "/package.json").version),
-            WEBPACK_ROOT: JSON.stringify(webpackRoot),
-            LOCALE_ROOT: JSON.stringify(appRoot + "/locale"),
-            THEME_ROOT: JSON.stringify(appRoot + "/theme"),
-        }),
-        new ExtractTextPlugin('css/[name].css', { allChunks: false}),
-        new ProvidePlugin({
-            _: 'lodash',
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-        new ResolverPlugin([new ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])], ["normal", "loader"]),
-        new DedupePlugin()
+    extensions: [
+      '',
+      '.js', '.coffee',
+      '.html', '.nunj',
+      '.css', '.scss', '.less',
+      '.json', '.yml'
     ]
-        // Setup each entry chunk to use a common chunk as defined in './app/webpack/config'.
-        .concat(commonsChunkPluginArr),
-    module: {
-        noParse: [
-            path.join(bowerRoot, "/lodash"),
-            path.join(bowerRoot, "/jquery"),
-            path.join(bowerRoot, "/bootstrap"),
-            path.join(bowerRoot, "/angular"),
-            path.join(bowerRoot, "/angular-ui-router"),
-            path.join(bowerRoot, "/angular-cookies"),
-            path.join(bowerRoot, "/angular-resource")
-        ],
-        loaders: [
-            {
-                test: /[\/]angular\.js$/,
-                loader: "exports?angular"
-            }, {
-                test: /jquery\.js$/,
-                loader: "expose?jQuery!expose?$"
-            }, {
-                test: /\.js?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    optional: ['runtime'],
-                    stage: 0,
-                    cacheDirectory: '/tmp'
-                }
-            }, {
-                test: /\.coffee$/,
-                loader: "coffee"
-            }, {
-                test: /\.json$/,
-                loader: "json"
-            }, {
-                test: /\.yml$/,
-                loader: "json!yaml"
-            }, {
-                test: /\.po$/,
-                loader: "json!po?format=jed1.x"
-            }, {
-                test: /\.html$/,
-                loader: "html"
-            }, {
-                test: /\.nunj$/,
-                loader: "nunjucks",
-                query: {
-                  config: `${devRoot}/nunjucks.config.js`,
-                  // Don't show the 'Cannot configure nunjucks environment before precompile' warning
-                  // This can be made quite if you understand the implications
-                  // quiet: true
-                }
-            }, {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader")
-            }, {
-                test: /\.less$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
-            }, {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
-            }, {
-                test: /\.(png|jpg|gif)$/,
-                loader: 'url?prefix=img/&limit=8192'
-            }, {
-                test: /\.(woff|woff2)$/,
-                loader: "url?name=fonts/[hash].[ext]&limit=10000&mimetype=application/font-woff"
-            }, {
-                test: /\.ttf$/,
-                loader: "file?name=fonts/[hash].[ext]"
-            }, {
-                test: /\.eot$/,
-                loader: "file?name=fonts/[hash].[ext]"
-            }, {
-                test: /\.svg$/,
-                loader: "file?name=fonts/[hash].[ext]"
-            }
-        ]
-    },
-    postcss: function (webpack) {
-      return [
-        require("postcss-import")({
-          addDependencyTo: webpack,
-          path: [themeRoot, appModulesRoot, nodeModulesRoot]
-        }),
-        require("postcss-url")(),
-        require("postcss-cssnext")({
-          // Defined in './app/webpack/config'
-          browsers: appConfig.browsers,
-          import: {
-            path: [themeRoot],
-            // Setup watches on these files
-            onImport: function(files) {
-              files.forEach(this.addDependency);
-            }.bind(this)
-          }
-        }),
-        require("postcss-browser-reporter")(),
-        require("postcss-reporter")(),
-      ]
-    }
+  },
+  resolveLoader: {
+    root: nodeModulesRoot
+  },
+  plugins: [
+    new DefinePlugin({
+      VERSION: JSON.stringify(require(devRoot + "/package.json").version),
+      WEBPACK_ROOT: JSON.stringify(webpackRoot),
+      LOCALE_ROOT: JSON.stringify(appRoot + "/locale"),
+      THEME_ROOT: JSON.stringify(appRoot + "/theme"),
+    }),
+    new ExtractTextPlugin('css/[name].css', { allChunks: false}),
+    new ProvidePlugin({
+      _: 'lodash',
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new ResolverPlugin([new ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])], ["normal", "loader"]),
+    new DedupePlugin()
+  ]
+  // Setup each entry chunk to use a common chunk as defined in './app/webpack/config'.
+  .concat(commonsChunkPluginArr),
+  module: {
+    noParse: [
+      path.join(bowerRoot, "/lodash"),
+      path.join(bowerRoot, "/jquery"),
+      path.join(bowerRoot, "/bootstrap"),
+      path.join(bowerRoot, "/angular"),
+      path.join(bowerRoot, "/angular-ui-router"),
+      path.join(bowerRoot, "/angular-cookies"),
+      path.join(bowerRoot, "/angular-resource")
+    ],
+    loaders: [
+      {
+        test: /[\/]angular\.js$/,
+        loader: "exports?angular"
+      }, {
+        test: /jquery\.js$/,
+        loader: "expose?jQuery!expose?$"
+      }, {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          optional: ['runtime'],
+          stage: 0,
+          cacheDirectory: '/tmp'
+        }
+      }, {
+        test: /\.coffee$/,
+        loader: "coffee"
+      }, {
+        test: /\.json$/,
+        loader: "json"
+      }, {
+        test: /\.yml$/,
+        loader: "json!yaml"
+      }, {
+        test: /\.po$/,
+        loader: "json!po?format=jed1.x"
+      }, {
+        test: /\.html$/,
+        loader: "html"
+      }, {
+        test: /\.nunj$/,
+        loader: "nunjucks",
+        query: {
+          config: `${devRoot}/nunjucks.config.js`,
+          // Don't show the 'Cannot configure nunjucks environment before precompile' warning
+          // This can be made quite if you understand the implications
+          // quiet: true
+        }
+      }, {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader")
+      }, {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      }, {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+      }, {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url?prefix=img/&limit=8192'
+      }, {
+        test: /\.(woff|woff2)$/,
+        loader: "url?name=fonts/[hash].[ext]&limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.ttf$/,
+        loader: "file?name=fonts/[hash].[ext]"
+      }, {
+        test: /\.eot$/,
+        loader: "file?name=fonts/[hash].[ext]"
+      }, {
+        test: /\.svg$/,
+        loader: "file?name=fonts/[hash].[ext]"
+      }
+    ]
+  },
+  postcss: function (webpack) {
+    return [
+      require("postcss-import")({
+        addDependencyTo: webpack,
+        path: [themeRoot, appModulesRoot, nodeModulesRoot]
+      }),
+      require("postcss-url")(),
+      require("postcss-cssnext")({
+        // Defined in './app/webpack/config'
+        browsers: appConfig.browsers,
+        import: {
+          path: [themeRoot],
+          // Setup watches on these files
+          onImport: function(files) {
+            files.forEach(this.addDependency);
+          }.bind(this)
+        }
+      }),
+      require("postcss-browser-reporter")(),
+      require("postcss-reporter")(),
+    ]
+  }
 };
 
 /**
  *
  */
 function getNamesFromDirectory(filepath) {
-    let files = fs.readdirSync(filepath);
-    let baseNames = _.chain(files).filter(function(filename) {
-        return filename[0] !== '#';
-    }).map(function(filename) {
-        let entryName, ext;
-        if (fs.lstatSync(filepath + "/" + filename).isDirectory()) {
-            entryName = filename;
-        } else {
-            ext = path.extname(filename);
-            entryName = filename.substr(0, filename.length - ext.length);
-        }
-        return entryName;
-    }).value();
-    return baseNames;
+  let files = fs.readdirSync(filepath);
+  let baseNames = _.chain(files).filter(function(filename) {
+    return filename[0] !== '#';
+  }).map(function(filename) {
+    let entryName, ext;
+    if (fs.lstatSync(filepath + "/" + filename).isDirectory()) {
+      entryName = filename;
+    } else {
+      ext = path.extname(filename);
+      entryName = filename.substr(0, filename.length - ext.length);
+    }
+    return entryName;
+  }).value();
+  return baseNames;
 };
 
 gulp.task('webpack:dev-server', function(callback) {
-    let config = yaml.load(fs.readFileSync(etcRoot + "/dev_defaults.yml", 'utf8'));
-    let configCustom = yaml.load(fs.readFileSync(etcRoot + "/dev.yml", 'utf8'));
-    _.merge(config, configCustom);
+  let config = yaml.load(fs.readFileSync(etcRoot + "/dev_defaults.yml", 'utf8'));
+  let configCustom = yaml.load(fs.readFileSync(etcRoot + "/dev.yml", 'utf8'));
+  _.merge(config, configCustom);
 
-    let webpackPort = config.dev.webpackPort;
-    wpConf.devtool = 'source-map';
-    wpConf.debug = true;
-    wpConf.plugins.push(new DefinePlugin({DEV: true}));
-    wpConf.output.publicPath = "http://" + config.site.domain[0] + "/";
+  let webpackPort = config.dev.webpackPort;
+  wpConf.devtool = 'source-map';
+  wpConf.debug = true;
+  wpConf.plugins.push(new DefinePlugin({DEV: true}));
+  wpConf.output.publicPath = "http://" + config.site.domain[0] + "/";
 
-    let devServer = new WebpackDevServer(webpack(wpConf), {
-        contentBase: devRoot,
-        stats: {
-            assets: false,
-            colors: true,
-            children: false,
-            chunks: false,
-            modules: false
-        }
-    }).listen(webpackPort, 'localhost', function(err) {
-        if (err) {
-            throw new gutil.PluginError('webpack-dev-server', err);
-        }
-    });
+  let devServer = new WebpackDevServer(webpack(wpConf), {
+    contentBase: devRoot,
+    stats: {
+      assets: false,
+      colors: true,
+      children: false,
+      chunks: false,
+      modules: false
+    }
+  }).listen(webpackPort, 'localhost', function(err) {
+    if (err) {
+      throw new gutil.PluginError('webpack-dev-server', err);
+    }
+  });
 });
 
 /**
  *
  */
 gulp.task('webpack:build', function(callback) {
-    wpConf.output.path = path.join(projectRoot, 'dist', 'public');
-    wpConf.plugins.concat([
-        new DefinePlugin({DEV: false}),
-        new UglifyJsPlugin()
-    ]);
+  wpConf.output.path = path.join(projectRoot, 'dist', 'public');
+  wpConf.plugins.concat([
+    new DefinePlugin({DEV: false}),
+    new UglifyJsPlugin()
+  ]);
 
-    webpack(wpConf, function(err, stats) {
-        if (err) {
-            throw new gutil.PluginError('webpack:build', err);
-        }
-        gutil.log('[webpack:build]', stats.toString({
-            colors: true
-        }));
-        callback();
-    });
+  webpack(wpConf, function(err, stats) {
+    if (err) {
+      throw new gutil.PluginError('webpack:build', err);
+    }
+    gutil.log('[webpack:build]', stats.toString({
+      colors: true
+    }));
+    callback();
+  });
 });

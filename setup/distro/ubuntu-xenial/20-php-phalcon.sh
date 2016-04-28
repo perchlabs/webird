15aba934e622d6f642181b2cef30132c44027462
David Schissler <david.schissler@gmail.com>
2016-04-29 02:07:18 +0300

    Change to get Phalcon 2.1 compiling on PHP 7.

--- setup/distro/ubuntu-xenial/20-php-phalcon.sh
+++ setup/distro/ubuntu-xenial/20-php-phalcon.sh
@@ -7,8 +7,13 @@ cd $TEMPDIR
 git clone --depth=1 -b 2.1.x git://github.com/phalcon/cphalcon.git
 [[ $? -ne 0 ]] && exit $?
 
-cd cphalcon/build
-./install
+git clone git://github.com/phalcon/zephir.git
+[[ $? -ne 0 ]] && exit $?
+cd zephir
+./install -c
+
+cd $TEMPDIR/cphalcon
+zephir build --backend=ZendEngine3
 [[ $? -ne 0 ]] && exit $?
 
 php-extension-enable 'phalcon' 50

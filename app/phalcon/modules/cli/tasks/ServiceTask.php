dd7937aff556721ed4014da3c4e6fb0ebef8a5c5
David Schissler <david.schissler@gmail.com>
2017-06-22 01:38:07 -0700

    Fix ZMQ socket socket type.

--- app/phalcon/modules/cli/tasks/ServiceTask.php
+++ app/phalcon/modules/cli/tasks/ServiceTask.php
@@ -37,12 +37,11 @@ class ServiceTask extends Task
     {
         $ctx = new ZMQContext();
 
-        $sub = $ctx->getSocket(ZMQ::SOCKET_SUB);
+        $sub = $ctx->getSocket(ZMQ::SOCKET_XSUB);
         $sub->bind("tcp://127.0.0.1:5554");
-        $sub->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, '');
         $sub->setSockOpt(ZMQ::SOCKOPT_LINGER, 0);
 
-        $pub = $ctx->getSocket(ZMQ::SOCKET_PUB);
+        $pub = $ctx->getSocket(ZMQ::SOCKET_XPUB);
         $pub->bind("tcp://127.0.0.1:5555");
         $pub->setSockOpt(ZMQ::SOCKOPT_LINGER, 0);
 

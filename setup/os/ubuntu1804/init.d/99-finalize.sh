a1c2b3e11e46c2f2144d5c243803ad628cf4fc1e
David Schissler <david.schissler@gmail.com>
2019-04-21 02:11:17 +0000

    Fix service restart bug, add Swoole PECL extension.

--- setup/os/ubuntu1804/init.d/99-finalize.sh
+++ setup/os/ubuntu1804/init.d/99-finalize.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Environment variables SETUP_ROOT_DIR, OS_DIR and TEMP_DIR are available
 
-sudo systemctrl restart nginx
-sudo systemctrl restart mariadb
-sudo systemctrl restart "php${PHP_VERSION}-fpm"
+sudo systemctl restart nginx
+sudo systemctl restart mariadb
+sudo systemctl restart "php${PHP_VERSION}-fpm"
